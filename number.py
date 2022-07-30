3f9c6c38c18cc137bd9f7a18af6bd4c2218e9d41
Mironshoh <mironshoh1545@gmail.com>
2022-07-29 22:43:37 -0700

    is_zero

--- number.py
+++ number.py
@@ -1,5 +1,9 @@
 class Number:
     def __init__(self,num):
-        pass
+        self.number = num
 
     # Create methods of Number class
+    def iszero(self):
+        if self.number == 0:
+            return True
+        return False

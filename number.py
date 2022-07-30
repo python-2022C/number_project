07bab523efc3314014bf98cfb8ba12911118a023
878 <majidovdiyorbek741@gmail.com>
2022-07-30 11:16:24 +0500

    num

--- number.py
+++ number.py
@@ -1,18 +1,11 @@
 import math
 class Number:
     def __init__(self,num):
-
         self.number = num
 
     # Create methods of Number class
-    def iszero(self):
-        if self.number == 0:
-            return True
-        return False 
-        return False
-        self.num = num
-
-
+    def is_zero(self):
+        return self.num==0
 
     # Create methods of Number class
     def length(self):
@@ -20,8 +13,7 @@ class Number:
         Returns length of number -> int
         """
         return len(str(self.num))
-    def len(self):
-        return len(str(self.num)) # -> int
+
     def is_positive(self):
         return self.num > 0 # if self.num > 0 return True ,otherwise return False
     def is_negative(self):
@@ -31,7 +23,7 @@ class Number:
     def is_odd(self):
         return self.num % 2 == 1 # if self.num is odd return True ,otherwise return False
     def is_prime(self):
-        for i in range(2, math.sqrt(self.num) +1):
+        for i in range(2, int(math.sqrt(self.num))+1):
             if self.num % i == 0:
                 return False 
         return True # if self.num is prime return True ,otherwise return False

d7df010d767069b5b943ed769e797f0ce85cc19b
KarimovProject <abdullatifkarimov772@gmail.com>
2022-07-30 10:49:23 +0500

    Number

--- number.py
+++ number.py
@@ -1,18 +1,16 @@
 class Number:
     def __init__(self,num):
         self.num = num
-    def length(self):
-        """
-        Returns length of number -> int
-        """
-        return len(str(self.num))
-    def data_type(self):
-        """
-        Returns data type of number -> int, float
-        """
+
+    # Create methods of Number class
+    # Data type determination
+    def typed(self):
         return type(self.num)
+    # Finding the length of a number
+    def length(self):
+        return len(str(self.num))
+    # determine whether it is positive
     def is_zero(self):
-        """
-        Returns given number is zero or non -> bool
-        """
         return self.num == 0
+
+        

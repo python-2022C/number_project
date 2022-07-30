ef0eeb8d75fc20fb96b0a5abb5244925c61f0357
tajriba <quvvatullayev@gmail.com>
2022-07-30 10:31:36 +0300

    .\number.py

--- number.py
+++ number.py
@@ -1,5 +1,11 @@
 class Number:
     def __init__(self,num):
-        pass
+        self.num = num
 
     # Create methods of Number class
+
+    def data_taype(self) ->type:
+        """
+        function that returns the type of the number
+        """
+        return (self.num)

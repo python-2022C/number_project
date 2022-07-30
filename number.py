ec0968b0e043393550802e7474112153fe30b5ac
Sherzod <daminovsherzod2002@gmail.com>
2022-07-30 10:33:04 +0500

    len

--- number.py
+++ number.py
@@ -1,5 +1,11 @@
 class Number:
     def __init__(self,num):
+        self.num = num
         pass
+    def len(self):
+        return len(str(self.num))
 
+'''
+    I found the length by changing the variable num to str.
+ '''
     # Create methods of Number class

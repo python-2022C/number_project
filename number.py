6d62f9edcbb526cbdd9efa7b5d7665f5fd6cf448
Sherzod <daminovsherzod2002@gmail.com>
2022-07-30 11:23:41 +0500

    I changed it

--- number.py
+++ number.py
@@ -26,13 +26,25 @@ class Number:
         """
         return self.num > 0 
     def is_negative(self):
-        return self.num < 0 # if self.num < 0 return True ,otherwise return False
+        return self.num < 0 
+        """
+        if self.num < 0 return True ,otherwise return False
+        """
     def is_even(self):
-        return self.num % 2 == 0 # if self.num is even return True ,otherwise return False
+        return self.num % 2 == 0 
+        """
+        if self.num is even return True ,otherwise return False
+        """
     def is_odd(self):
-        return self.num % 2 == 1 # if self.num is odd return True ,otherwise return False
+        return self.num % 2 == 1 
+        """
+        if self.num is odd return True ,otherwise return False
+        """
     def is_prime(self):
         for i in range(2, math.sqrt(self.num) +1):
             if self.num % i == 0:
                 return False 
-        return True # if self.num is prime return True ,otherwise return False
+        return True 
+        """
+        if self.num is prime return True ,otherwise return False
+        """
